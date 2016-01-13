dd65aaec4eaf15b7ddaa1d5fe8d9b67791121e79
KevGary <kevgary.dev@gmail.com>
2016-01-13 03:03:57 -0700

    nav tracker working, with html5 mode though

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,8 +23,30 @@ app.filter('reverse', function() {
 
 //on body tag in index.html
 app.controller('GlobalController', function($scope, $location) {
-  $scope.currentTab = $location.path().split('/')[1];
-  console.log($scope.currentTab);
+  $scope.currentTab = $location.path();
+  var start = '';
+  if($scope.currentTab == '/') {
+    start = '.root-nav';
+  }
+  if($scope.currentTab == '/editor') {
+    start = '.editor-nav';
+  }
+  if($scope.currentTab == '/hosting') {
+    start = '.hosting-nav';
+  }
+  if($scope.currentTab == '/docs') {
+    start = '.docs-nav';
+  }
+  if($scope.currentTab == '/metrics') {
+    start = '.metrics-nav';
+  }
+  // $('li').css({"border-bottom": "none"});
+  $(start).css({"border-bottom": "2px solid #dddddd"});  
+  $('li').on('click', function() {
+    $(start).css({"border-bottom": "none"});
+    $('li').css({"border-bottom": "none"});
+    $(this).css({"border-bottom": "2px solid #dddddd"});
+  })
 })
 
 app.controller('LandingController', function($scope) {
@@ -197,5 +219,5 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     // .otherwise({
     //   redirectTo: '/page-not-found'
     // });
-  // $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 }]);
